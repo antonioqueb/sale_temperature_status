42eed843af5afbb4d92c8c8a8c441ceae149d20f
Antonio Queb <antonioqueb@alphaqueb.online>
2025-01-28 10:48:14 -0600

    Up

--- models/sale_order.py
+++ models/sale_order.py
@@ -8,28 +8,20 @@ class SaleOrder(models.Model):
         compute='_compute_temperature_status',
         store=False
     )
-    temperature_class = fields.Char(
-        string='Class Color',
-        compute='_compute_temperature_status',
-        store=False
-    )
 
     @api.depends('create_date', 'commitment_date')
     def _compute_temperature_status(self):
         for order in self:
             if not order.create_date or not order.commitment_date:
                 order.temperature_status = False
-                order.temperature_class = ''
                 continue
             
             create_date = order.create_date.date()
             delivery_date = order.commitment_date.date()
             delta = (delivery_date - create_date).days
             
-            # Asegura que los nombres de clase coincidan con el CSS
+            # Asignar valores según el delta de días
             if delta < 15:
                 order.temperature_status = 'Alta'
-                order.temperature_class = 'temperature-badge-danger'
             else:
                 order.temperature_status = 'Baja'
-                order.temperature_class = 'temperature-badge-success'
