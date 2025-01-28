1555eb9c17bba6e939813b035ce22074f36230f6
Antonio Queb <antonioqueb@alphaqueb.online>
2025-01-27 20:42:11 -0600

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Sale Temperature Status',
+    'version': '1.0',
+    'summary': 'Muestra un indicador de temperatura en órdenes de venta',
+    'category': 'Sales',
+    'author': 'Alphaqueb Consulting S.A.S.',
+    'website': 'https://alphaqueb.com',
+    'depends': ['sale_management'],
+    'data': [
+        'views/sale_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
---
+++ models/sale_order.py
@@ -0,0 +1,24 @@
+from odoo import models, fields, api
+from datetime import datetime
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    temperature_status = fields.Char(
+        string='Temperatura',
+        compute='_compute_temperature_status',
+        store=False
+    )
+
+    @api.depends('create_date', 'commitment_date')
+    def _compute_temperature_status(self):
+        for order in self:
+            if not order.create_date or not order.commitment_date:
+                order.temperature_status = False
+                continue
+            
+            create_date = order.create_date.date()
+            delivery_date = order.commitment_date
+            delta = (delivery_date - create_date).days
+            
+            order.temperature_status = 'green' if delta >= 15 else 'red'
